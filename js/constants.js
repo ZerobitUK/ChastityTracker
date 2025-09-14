ca6a2278cd18fc929fbaa1a9e605aacc481b109f
ZerobitUK <william_mcdonald@gmx.com>
2025-09-14 20:51:16 +0100

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -43,9 +43,10 @@ export const RANDOM_EVENTS = [
 export const WHEEL_OUTCOMES = [
     { text: '+1 Hour', type: 'addTime', value: 60 * 60 * 1000 },
     { text: 'Play Game', type: 'play' },
-    { text: '-15 Mins', type: 'subtractTime', value: 15 * 60 * 1000 },
+    // UPDATE THIS LINE
+    { text: '+15 Mins', type: 'addTime', value: 15 * 60 * 1000 },
     { text: 'Double Penalty on Loss', type: 'modifier', effect: 'doublePenalty' },
-    { text: 'Lockdown!', type: 'lockdown' }, // <-- ADD THIS LINE
+    { text: 'Lockdown!', type: 'lockdown' },
     { text: 'Free Unlock Attempt', type: 'play', free: true },
     { text: 'Safe... for now', type: 'nothing' },
     { text: '+3 Hours', type: 'addTime', value: 3 * 60 * 60 * 1000 },
