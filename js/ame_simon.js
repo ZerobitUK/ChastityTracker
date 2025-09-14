fd28c112d3df69791102a10115048dfde8d30423
ZerobitUK <william_mcdonald@gmx.com>
2025-09-14 18:18:07 +0100

    Create ame_simon.js

---
+++ js/ame_simon.js
@@ -0,0 +1,10 @@
+let sequence, playerSequence, level;
+const WIN_LEVEL = 5;
+let onWin, onLose;
+// ... DOM element gets ...
+
+function nextSequence() { /* ... */ }
+function playSequence() { /* ... */ }
+function handleButtonClick(event) { /* ... */ }
+
+export function initSimonSays(winCallback, loseCallback) { /* ... setup ... */ }
