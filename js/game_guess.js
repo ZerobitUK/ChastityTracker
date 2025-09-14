c7248ba2af343e6b44be865075a3ba2108c73509
ZerobitUK <william_mcdonald@gmx.com>
2025-09-14 18:17:52 +0100

    Create game_guess.js

---
+++ js/game_guess.js
@@ -0,0 +1,8 @@
+let secretNumber, attempts;
+const MAX_ATTEMPTS = 5;
+let onWin, onLose;
+// ... DOM element gets ...
+
+function checkGuess() { /* ... logic for guess ... */ }
+
+export function initGuessTheNumber(winCallback, loseCallback) { /* ... setup ... */ }
