effb75c568cb010559d137258073b599fffa0ac5
ZerobitUK <william_mcdonald@gmx.com>
2025-09-15 01:14:07 +0100

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -67,7 +67,9 @@ export function startUpdateInterval() {
             const timeLeft = currentTimer.minEndTime - now;
             const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
             const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            updateTimerMessage(`Minimum Time: ${days}d ${hours}h remaining.`);
+            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+            updateTimerMessage(`Minimum Time Remaining: ${days}d ${hours}h ${minutes}m ${seconds}s`);
             toggleUnlockButton(false);
             return;
         }
